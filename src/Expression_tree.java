c3d5a8e649b3564ad3723759dbe78e57b6227537
Dcastroe10 <85363712+Dcastroe10@users.noreply.github.com>
2021-11-04 01:16:30 -0600

    Updating progress

--- src/Expression_tree.java
+++ src/Expression_tree.java
@@ -1,42 +1,133 @@
+import java.util.Stack;
+
 public class Expression_tree {
 
-    public Expression_tree(String expression){
-        String data1 = "";
-        String operator = "";
-        String data2 = "";
-        int cambiar_esto = 1;
-        //System.out.println("ENTRÓOOOOOOO");
-        //System.out.println(expression);
-
-        for (int i =1; i<expression.length();i++){
-            char daam = expression.charAt(i);
-            if(!decode(daam) && cambiar_esto<2){
-                String result = "";
-                for (int n = 0; n<expression.length();n++){
-                    if (!decode(expression.charAt(n))){
-                        result += expression.charAt(n);
-                    }else{
+    public Expression_tree(String postfix){
+        Stack<Node> stack = new Stack<Node>();
+        int indice = 0;
+        System.out.println(postfix);
+        while (indice < postfix.length()) {
+            Node nodo = new Node();
+            if (postfix.charAt(indice) != 'P' && !operador(postfix.charAt(indice))) {
+                String number = "";
+                for (int i = indice; i < postfix.length(); i++) {
+                    if (postfix.charAt(i) != 'P' && !operador(postfix.charAt(i))) {// && !operador(postfix.charAt(i))) {
+                        number += postfix.charAt(i);
+                    }
+                    if (postfix.charAt(i) == 'P') {
                         break;
                     }
+                    indice = i;
                 }
-                System.out.println(data1);
-                cambiar_esto++;
-                data1=result;
-                System.out.println(data1);
+                while (postfix.charAt(indice) != 'P') {
+                    indice += 1;
+                }
+                nodo.setData(number);
+                stack.push(nodo);
+                //System.out.println("PUSHEDDDDDDD");
+                //System.out.println(nodo.getData());
+                continue;
             }
 
 
-
-
-
+            if (operador(postfix.charAt(indice))){
+                nodo.setData(postfix.charAt(indice));
+                //System.out.println(nodo.getData()+" DATOS DEL NODO OPERADOR");
+                //System.out.println(stack.peek().getData());
+                //System.out.println(stack.peek().getData());
+                nodo.setRight(stack.pop());
+                nodo.setLeft(stack.pop());
+                stack.push(nodo);
+            }
+            indice++;
         }
+        System.out.println(stack.peek().getData()+ "ROOOT");
 
-
+        Node root = stack.pop();
+        //pruebas
+        /*
+        System.out.println("EEEEEEEEEE");
+        System.out.println(pepe.getData());
+        System.out.println(pepe.getLeft().getLeft().getData()+ "LEFTTT");
+        System.out.println(pepe.getLeft().getRight().getData()+ "LEFTTT");
+        System.out.println(pepe.getRight().getData()+"RIIIGHT");
+        System.out.println("SE CELEBRA");
+        //return nodo;
+         */
+        System.out.println("EMPIEZA LA SOLUCIÓN");
+        //recorrer(root);
+        solve(root);
 
 
     }
 
-    public boolean decode(char f){
+    public void recorrer(Node root){
+        String result ="";
+        if (root!=null){
+            recorrer(root.getLeft());
+            recorrer(root.getRight());
+            System.out.println(root.getData());
+            result+=root.getData();
+        }
+    }
+
+    public int solve(Node root){
+        Stack<Integer> stack = new Stack<Integer>();
+        int result=0;
+        if (root!= null && root.getLeft().getType()=="number" && root.getRight().getType()=="number"){
+            System.out.println("EL CAREPICHA ENTRÓ");
+            String left = String.valueOf(root.getLeft().getData());
+            int operando_left =Integer.valueOf(left);
+            String right = String.valueOf(root.getRight().getData());
+            int operando_right = Integer.valueOf(right);
+
+            System.out.println(operando_left);
+            System.out.println(operando_right + "operador de la derecha");
+            String tipo = root.getType();
+            System.out.println(tipo);
+            if (tipo.equals("+")){
+                result+= operando_left + operando_right;
+                System.out.println("FUCK");
+            }
+            if (tipo.equals("+")){
+                result+= operando_left + operando_right;
+                System.out.println("FUCK");
+            }
+            if (tipo.equals("-")){
+                result+= operando_left - operando_right;
+                System.out.println("FUCK");
+            }
+            if (tipo.equals("*")){
+                result+= operando_left * operando_right;
+                System.out.println("FUCK");
+            }
+            if (tipo.equals("/")){
+                result+= operando_left / operando_right;
+                System.out.println("FUCK");
+            }
+            if (tipo.equals("%")){
+                result+= operando_left % operando_right;
+                System.out.println("FUCK");
+            }
+        System.out.println(result);
+        }
+        if (root != null && root.getLeft().getData() != "number" && root.getRight().getData()!= "number" ) {
+            System.out.println("PRAY GOD");
+            String tipo = root.getType();
+            System.out.println(tipo);
+            if (tipo.equals("+")) {
+                System.out.println("THANKS GOD");
+                System.out.println(root.getLeft().getData());
+                result += solve(root.getLeft()) + solve(root.getRight());
+            }
+
+        }
+    System.out.println("El resultado final es: "+result);
+    return result;
+    }
+
+
+    public boolean operador(char f){
         if (f == '+'){
             return true;
         }else if (f == '-'){
@@ -47,18 +138,10 @@ public class Expression_tree {
             return true;
         }else if (f == '%'){
             return true;
-        }else if (f == 'P') {
-            return true;
-        }else {
+        }else{
             return false;
         }
     }
-
-
-
-
-
-
 
 
 }
--- src/Sub_trees.java
+++
@@ -1,7 +0,0 @@
-public class Sub_trees {
-    public Node Sub_trees(Node root, Node left, Node right){
-        root.setLeft(left);
-        root.setRight(right);
-        return root;
-    }
-}
